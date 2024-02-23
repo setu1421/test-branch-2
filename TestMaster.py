caca37a5ae51314cf68ccf1f4cc4b1cab9258e25
Setu Kumar Basak <setu.1421@yahoo.com>
2024-02-23 18:12:09 -0500

    Update TestMaster.py

--- TestMaster.py
+++ TestMaster.py
@@ -1,3 +1,5 @@
-ApiKey = "AIzaSyCawgkS1q9YasaZwOk9n2Q8VZY_GFwO24M"
-def use():
-  pass
+public class KinesisComponentIntegrationTest extends CamelTestSupport {
+
+    @BindToRegistry("amazonKinesisClient")
+    AmazonKinesis client = AmazonKinesisClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
+    AmazonKinesis client = AmazonKinesisClientBuilder.standard().withRegion(Regions.EU_WEST_1).withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIAIHMCLOA573Z7SKYQ", "DC1Ow3nyac3/2OHV7zLl57oC7hnLPOB1BTtrUQGI"))).build();
